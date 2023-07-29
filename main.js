831d0daefdb88942e1b406c2fb2f3168f3ba4eed
Alham <alhammanazil18@gmail.com>
2023-07-29 19:37:18 +0700

    deactive main.js

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-const visit = prompt("Apakah anda pernah pergi berkunjung ke kota kudus? (pernah / belum)");
-const firstName = prompt("Silahkan masukkan nama anda");
+// const visit = prompt("Apakah anda pernah pergi berkunjung ke kota kudus? (pernah / belum)");
+// const firstName = prompt("Silahkan masukkan nama anda");
 
-const user = {
-    visit: visit,
-    firstName: firstName,
-};
+// const user = {
+//     visit: visit,
+//     firstName: firstName,
+// };
  
-if (user.visit === "pernah") {
-   alert("Hai " + user.firstName + " " + "senang bertemu dengan anda, pada website ini mungkin ada tempat yang ingin ada kunjungi");
-} else if (user.visit === "belum") {
-    alert("Hai " + user.firstName + " " + "senang bertemu dengan anda, website ini membahas tentang kota kudus yang berada di kabupaten jawa tengah dan mungkin ada tempat yang ingin anda kunjungi");
-} else {
-   alert("Senang bertemu dengan anda " + user.firstName);
-}
+// if (user.visit === "pernah") {
+//    alert("Hai " + user.firstName + " " + "senang bertemu dengan anda, pada website ini mungkin ada tempat yang ingin ada kunjungi");
+// } else if (user.visit === "belum") {
+//     alert("Hai " + user.firstName + " " + "senang bertemu dengan anda, website ini membahas tentang kota kudus yang berada di kabupaten jawa tengah dan mungkin ada tempat yang ingin anda kunjungi");
+// } else {
+//    alert("Senang bertemu dengan anda " + user.firstName);
+// }
